47b80cdb3cf6d709c8c303e0fb6537ae491a3b54
Khushboo Moolchandani <mool0009@algonquinlive.com>
2017-01-23 13:09:24 -0500

    update looper.js

--- js/looper.js
+++ js/looper.js
@@ -8,7 +8,7 @@ console.log("The character is " + letter + "!");
 
 var i, k;
 for (i = 0; i < 10; i +=1) {	
-	for(k = 0; k < i; k +=1 ){
+	for(k = 0; k <= i; k +=1 ){
 		document.write(letter);
 	}
 document.write("<br>");
