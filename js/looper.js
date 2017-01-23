fbe4f41eb5085ce8802e583b6408714c6d8fc85f
Khushboo Moolchandani <mool0009@algonquinlive.com>
2017-01-23 13:01:46 -0500

    index and js files uploaded

---
+++ js/looper.js
@@ -0,0 +1,15 @@
+//Declare the variable (letter for example) which will store a letter of user's choice (don't assign any value to it just yet).
+
+var letter
+while (!letter){
+	letter = prompt ("Enter a character!");
+}
+console.log("The character is " + letter + "!");
+
+var i, k;
+for (i = 0; i < 10; i +=1) {	
+	for(k = 0; k < i; k +=1 ){
+		document.write(letter);
+	}
+document.write("<br>");
+}
